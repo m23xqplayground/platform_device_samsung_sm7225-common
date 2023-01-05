c3c8a11d796b7f2fa84d519c6f5c2dbe8274838f
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-01-05 11:39:56 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -390,6 +390,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3 \
+    android.hardware.usb@1.3.vendor \
     android.hardware.usb@1.3-service-qti
 
 # Vibrator
