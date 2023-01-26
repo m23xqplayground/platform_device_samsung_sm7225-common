384ce3826e3af127e8fc3f3c8147282b6a27b4fa
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-01-25 21:13:17 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -67,8 +67,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@4.0-impl \
