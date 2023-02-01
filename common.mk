892bb015feb8d3a00502b62dd3c60e553aca28b7
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-02-01 19:31:24 -0300

    common.mk: Remove drm@1.4.vendor
    File doesn't exist

--- common.mk
+++ common.mk
@@ -180,8 +180,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     libdrmclearkeyplugin \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4.vendor
+    android.hardware.drm@1.3.vendor
 
 # Fingerprint
 PRODUCT_PACKAGES += \
