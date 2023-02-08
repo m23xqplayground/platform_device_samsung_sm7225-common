71de2505a4031df4841505d3659bb7d2eaf637ae
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-02-07 21:06:55 -0300

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -63,15 +63,4 @@ BLOB_ROOT="$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
 # Replace libutils with vndk30 libutils
 "${PATCHELF}" --replace-needed libutils.so libutils-v30.so $BLOB_ROOT/vendor/lib64/hw/android.hardware.health@2.0-impl-2.1-samsung.so
 
-# Fix patch camera
-BLOB_ROOT="$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-
-function blob_fixup() {
-    case "${1}" in
-	vendor/lib64/hw/camera.qcom.so)
-	    sed -i 's/ro.boot.flash.locked/ro.camera.notify_nfc/g' "${2}"
-	    ;;
-    esac
-}
-
 "${MY_DIR}/setup-makefiles.sh"
