232f1ad748744a8448ec24e72b9d1eea3eaf748d
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-02-16 12:51:37 -0300

    common.mk: Drop Samsung thermal and switch to mock

--- common.mk
+++ common.mk
@@ -420,7 +420,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0.vendor
+    android.hardware.thermal@2.0-service.mock
 
 # VNDK
 PRODUCT_COPY_FILES += \
