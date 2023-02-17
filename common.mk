61eb0bef12776a0f8773746cdf71895e1c8b2922
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-02-17 14:25:12 -0300

    common.mk: Uprev android.hardware.bluetooth.audio

--- common.mk
+++ common.mk
@@ -111,10 +111,12 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    libbthost_if \
+    libbthost_if.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.a2dp@1.0-impl \
     android.hardware.bluetooth.a2dp@1.0-service
