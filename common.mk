ef89503aa23c02c8376e3f38fc4197877e869d2d
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-02-17 14:23:13 -0300

    common.mk: Add libstdc++ libs

--- common.mk
+++ common.mk
@@ -67,13 +67,14 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@4.0-impl \
+    vendor.samsung.hardware.audio@2.0-impl \
+    vendor.samsung.hardware.audio@4.0-impl \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.3-impl \
     audioadsprpcd \
     audio.primary.default \
+    libstdc++.vendor \
     audio.primary.lito \
     audio.a2dp.default \
     audio.r_submix.default \
