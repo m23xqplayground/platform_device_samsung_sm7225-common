e1ba2b8099944a965488766f2f5b688b0b03c33d
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-03-10 00:40:22 -0300

    common.mk: Add more Camera stuff

--- common.mk
+++ common.mk
@@ -125,6 +125,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.6-service.sm7225 \
     android.hardware.camera.provider@2.4-legacy \
     android.hardware.camera.provider@2.5-legacy \
     android.hardware.camera.provider@2.6.vendor \
@@ -133,6 +134,8 @@ PRODUCT_PACKAGES += \
     camera.device@3.3-impl \
     camera.device@3.4-impl \
     camera.device@3.5-impl \
+    libgrallocusage.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor
     android.hardware.camera.device@3.6.vendor \
     android.frameworks.cameraservice.service@2.1.vendor
 
