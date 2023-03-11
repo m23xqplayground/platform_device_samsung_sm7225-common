bf4f45689bb5ec3183a25b02ade903b8eb48148a
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-03-11 00:38:29 -0300

    common.mk: Build android.hardware.power.stats

--- common.mk
+++ common.mk
@@ -381,7 +381,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power@1.3.vendor
+    android.hardware.power@1.3.vendor \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
