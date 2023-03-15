867a312bf6ad7497c787f93d91565a2eb69b5fff
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-03-15 02:01:24 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -512,9 +512,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl
