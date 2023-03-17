2533c7fa9fd22e72ed9fc35ae7031ff759f34db4
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-03-17 03:45:00 -0300

    common.mk: Add Gadget HAL 1.0
    Solves issue:
    
    USB GADGET HAL not present in the device

--- common.mk
+++ common.mk
@@ -441,7 +441,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3 \
     android.hardware.usb@1.3.vendor \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service-qti \
+    android.hardware.usb.gadget@1.0-impl \
+    android.hardware.usb.gadget@1.0-service
 
 # Vibrator
 PRODUCT_PACKAGES += \
