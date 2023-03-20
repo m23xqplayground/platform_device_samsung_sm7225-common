951e2a50a425103ba9f3f06517f3ca37c4405a4c
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-03-20 00:17:38 -0300

    Android.mk: Fix duplicate

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -53,14 +53,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.audio.samsung.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.audio.samsung.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsung.bsp.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
