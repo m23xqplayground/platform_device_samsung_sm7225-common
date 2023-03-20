98cb875937232a98cda554a6d9e13ad643ff0974
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-03-20 00:42:18 -0300

    Android.mk: Removed init.qti.media.sh

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -304,14 +304,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qti.media.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qti.media.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qti.qcv.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
