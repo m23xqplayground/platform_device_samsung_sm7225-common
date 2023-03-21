c8578d562d9474749820bbbb23d7e88b7ecb3b8a
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-03-21 11:34:33 -0300

    common.mk: Removed sensors.samsung

--- common.mk
+++ common.mk
@@ -412,8 +412,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.samsung-multihal \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    sensors.samsung
+    android.hardware.sensors@2.0-ScopedWakelock.vendor
 
 # Touch features
 PRODUCT_PACKAGES += \
