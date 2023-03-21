eb6d303b60b38145fa91fa4b2ead67c90712b67d
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-03-21 11:28:14 -0300

    common.mk: Removed some USB HALs

--- common.mk
+++ common.mk
@@ -429,8 +429,6 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3 \
-    android.hardware.usb@1.3.vendor \
     android.hardware.usb@1.3-service-qti
 
 # Vibrator
