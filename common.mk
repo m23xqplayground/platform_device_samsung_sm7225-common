61cbb049a65dd3a6ba59f54010c4fa5397e25c2b
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-03-20 23:17:13 -0300

    common.mk: Add back old namespaces

--- common.mk
+++ common.mk
@@ -512,8 +512,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
+    hardware/samsung \
     hardware/samsung/aidl/power-libperfmgr
 
 # Prop files
