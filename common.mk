920c80595b2b44f71655ffd96efd0a2f5bcc7987
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-03-24 12:16:00 -0300

    common.mk: Fix Display

--- common.mk
+++ common.mk
@@ -158,7 +158,6 @@ PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    vendor.qti.hardware.display.allocator-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
@@ -166,14 +165,18 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0.vendor \
     android.hardware.graphics.mapper@2.1.vendor \
     android.hardware.graphics.mapper@3.0.vendor \
+    gralloc.default \
+    libsdmcore \
+    libsdmutils \
     libtinyxml \
-    libtinyxml2 \
     libvulkan \
     hwcomposer.lito \
     gralloc.lito \
     memtrack.lito \
     libqdMetaData \
+    libqdMetaData.system \
     libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.mapper@1.0.vendor \
@@ -181,19 +184,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \
-    vendor.display.config@1.0.vendor \
-    vendor.display.config@1.1.vendor \
-    vendor.display.config@1.2.vendor \
-    vendor.display.config@1.3.vendor \
-    vendor.display.config@1.4.vendor \
-    vendor.display.config@1.5.vendor \
-    vendor.display.config@1.6.vendor \
-    vendor.display.config@1.7.vendor \
-    vendor.display.config@1.8.vendor \
-    vendor.display.config@1.9.vendor \
-    vendor.display.config@1.10.vendor \
+    vendor.display.config@1.11 \
     vendor.display.config@1.11.vendor \
-    vendor.display.config@2.0.vendor
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor \
+    AdvancedDisplay
 
 # Doze
 PRODUCT_PACKAGES += \
