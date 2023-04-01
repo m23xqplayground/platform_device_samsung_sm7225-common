f62a1b154111226c01683fe74cec23c6077c8377
Aflaungos <vimavick@gmail.com>
2023-04-01 13:01:23 -0300

    Restore Power config

--- common.mk
+++ common.mk
@@ -378,7 +378,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.samsung-libperfmgr \
+    android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
@@ -523,8 +523,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
+    hardware/samsung \
     hardware/samsung/aidl/power-libperfmgr
 
 # Prop files
