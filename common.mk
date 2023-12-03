1d6cd51229a89cbfa1fca749257385d52af267a3
Aflaungos <vimavick@gmail.com>
2023-12-02 22:39:53 -0300

    common.mk: Fix?

--- common.mk
+++ common.mk
@@ -191,10 +191,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.fastcharge@1.0-service.samsung
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # FlipFlap
 PRODUCT_PACKAGES += \
     FlipFlap
