7ce88d5119af9221d5c422f9a5f168372fe00b35
Aflaungos <vimavick@gmail.com>
2023-12-04 18:53:01 -0300

    Revert "common.mk: Remove google namespaces"
    This reverts commit bb62b18d0b161637fbd7d1c19cbd210543be6ea8.

--- common.mk
+++ common.mk
@@ -433,6 +433,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/samsung \
     hardware/samsung/aidl/power-libperfmgr
 
