bb62b18d0b161637fbd7d1c19cbd210543be6ea8
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-12-04 18:30:43 -0300

    common.mk: Remove google namespaces

--- common.mk
+++ common.mk
@@ -433,8 +433,6 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
     hardware/samsung \
     hardware/samsung/aidl/power-libperfmgr
 
