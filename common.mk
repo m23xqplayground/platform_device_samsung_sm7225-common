838d31005ea1a774ea45d1d0de16fa036e4b027b
Aflaungos <vimavick@gmail.com>
2023-12-06 23:21:09 -0300

    Set USB OSS again

--- common.mk
+++ common.mk
@@ -362,6 +362,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
 
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
