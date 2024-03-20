dfe70d4dc618990ce3a8cd3bebb55571e2c1ab55
Tim Zimmermann <tim@linux4.de>
2024-03-20 16:53:40 -0300

    sm7225-common: Uprev audio HAL to V7
    Updated audio policy with some manual intervention.

--- common.mk
+++ common.mk
@@ -70,8 +70,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl.samsung-sm7225 \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl.samsung-sm7225 \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.r_submix.default \
     audio.usb.default \
