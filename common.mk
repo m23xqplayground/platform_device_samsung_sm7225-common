2c2092c74ea03fb76988408cbf3b5b431b60b127
Aflaungos <vimavick@gmail.com>
2024-03-21 17:36:18 -0300

    Adjust Display HALs

--- common.mk
+++ common.mk
@@ -143,29 +143,32 @@ PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    vendor.qti.hardware.display.allocator-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
-    android.hardware.graphics.composer@2.1.vendor \
-    android.hardware.graphics.composer@2.2.vendor \
-    android.hardware.graphics.composer@2.3.vendor \
-    android.hardware.graphics.composer@2.4.vendor \
+    android.hardware.graphics.composer@2.1 \
     android.hardware.graphics.mapper@2.0.vendor \
     android.hardware.graphics.mapper@2.1.vendor \
     android.hardware.graphics.mapper@3.0.vendor \
+    libsdmcore \
+    libsdmutils \
     libtinyxml \
     libtinyxml2 \
+    gralloc.lito \
     gralloc.default \
     memtrack.lito \
     libqdMetaData \
+    libqdMetaData.system \
     libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.mapper@1.0.vendor \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \
-    vendor.display.config@2.0.vendor \
-    android.hardware.graphics.composer@2.1-service
+    vendor.display.config@2.0.vendor
 
 # Doze
 PRODUCT_PACKAGES += \
