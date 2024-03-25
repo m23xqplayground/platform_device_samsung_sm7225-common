a9701971606d7b6b806a092e6e264ec96bb26dcb
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-03-25 13:38:54 -0300

    common.mk: Add sensors.samsung

--- common.mk
+++ common.mk
@@ -365,7 +365,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.samsung-multihal \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    sensors.samsung
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
