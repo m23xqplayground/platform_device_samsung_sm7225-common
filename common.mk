250b653d2bf6f5a81270e9dd4701465aff4eb82e
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-03-27 21:46:51 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -132,10 +132,10 @@ PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
