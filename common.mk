6b9e6f3a728b035369d1f08d804661f4d9ef6cbc
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-03-30 18:15:32 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -136,7 +136,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.graphics.composer@2.3-service-sm7250 \
+    android.hardware.graphics.composer@2.4-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
