ac324e33b9d10538bedc34dbefb6f1f1cda6d80c
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-06 20:39:46 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -268,7 +268,6 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.samsung \
-    android.hardware.nfc@1.0-impl \
     com.android.nfc_extras \
     NfcNci \
     Tag
