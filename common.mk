791355beb12ae07ab3ec5d0aafa0d33a01e0f03f
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-06 18:57:10 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -102,6 +102,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6-service.sm7225 \
     android.hardware.camera.provider@2.6.vendor \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    camera.device@3.3-impl \
+    camera.device@3.4-impl \
+    camera.device@3.5-impl \
     libgrallocusage.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     android.hardware.camera.device@3.6.vendor
