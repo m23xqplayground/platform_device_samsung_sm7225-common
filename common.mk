0d2d077a713b65b5e2ab87ccaba61bdf350d4625
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-06 20:38:53 -0300

    common.mk: Revert Fingerprint

--- common.mk
+++ common.mk
@@ -151,8 +151,9 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
-# fastbootd
+    android.hardware.biometrics.fingerprint@2.3-service.samsung
+
+# Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
 
