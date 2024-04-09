1ac2229bcd547214e48b0028b1cbf2dd6ed79761
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-09 12:34:48 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -285,7 +285,6 @@ PRODUCT_COPY_FILES += \
 
 # NFC Permissions
 PRODUCT_COPY_FILES += \
-    PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
