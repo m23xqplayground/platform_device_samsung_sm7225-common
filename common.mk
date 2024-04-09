74212cb65d945ae66c5b1f9f1afbe6df3ce18b66
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-09 14:10:18 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -395,14 +395,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-
 # Shims
 PRODUCT_PACKAGES += \
     sysinputshim
-
-# Touch features
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.sm7225
 
 # Trust HAL
 PRODUCT_PACKAGES += \
