9649ea7a7083b6c2d8be5b637cdc6cddbe01e68b
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-11 11:36:16 -0300

    Delete libshims directory

--- libshims/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := sysinputshim.cpp
-LOCAL_MODULE := sysinputshim
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
--- libshims/sysinputshim.cpp
+++
@@ -1,3 +0,0 @@
-extern "C" {
-   void _ZN6vendor7samsung8hardware8sysinput4V1_315ISehSysInputDev14interfaceChainENSt3__18functionIFvRKN7android8hardware8hidl_vecINS8_11hidl_stringEEEEEE() {}
-}
