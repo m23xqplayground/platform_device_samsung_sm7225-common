857157af7f08ed33e81ab37e37a9ad43a7584ceb
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-12 23:40:25 -0300

    common.mk: Switch back to Samsung Fingerprint

--- common.mk
+++ common.mk
@@ -162,7 +162,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.3-service.samsung
 
 # Fastbootd
 PRODUCT_PACKAGES += \
