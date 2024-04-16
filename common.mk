a0368a1dd46cfdb9b153bd41781a73d78c4d0c8f
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-16 19:33:51 -0300

    common.mk: Add android.hardware.biometrics.fingerprint@2.1

--- common.mk
+++ common.mk
@@ -164,6 +164,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+   android.hardware.biometrics.fingerprint@2.1 \
    android.hardware.biometrics.fingerprint@2.1.vendor
 
 # Fastbootd
