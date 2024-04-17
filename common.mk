235bb7c23f1730ca3e98ee97b5e7ce710ef08cc1
Aflaungos <vimavick@gmail.com>
2024-04-17 11:00:15 -0300

    common.mk: Use Fingerprint AIDL from hardware/samsung

--- common.mk
+++ common.mk
@@ -166,8 +166,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-   android.hardware.biometrics.fingerprint@2.1 \
-   android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint-service.samsung
 
 # Fastbootd
 PRODUCT_PACKAGES += \
