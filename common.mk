5d55896ecda9bf59bdf07d9215378b2135b4eb34
Aflaungos <vimavick@gmail.com>
2024-04-24 21:10:22 -0300

    Use Our own camera HAL

--- common.mk
+++ common.mk
@@ -102,19 +102,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.7.vendor \
-    android.hardware.camera.common@1.0.vendor \
-    android.frameworks.cameraservice.service@2.0.vendor \
-    android.frameworks.cameraservice.service@2.1.vendor \
-    android.frameworks.cameraservice.service@2.2.vendor \
-    android.hardware.camera.device-V1-ndk.vendor \
-    android.hardware.camera.metadata-V1-ndk.vendor \
-    android.hardware.camera.provider-V1-ndk.vendor \
-    libcamera2ndk_vendor \
+    android.hardware.camera.provider@2.6-service.sm7225 \
     libgrallocusage.vendor \
-    libmeminfo.vendor:64 \
-    vendor.qti.hardware.camera.aon@1.3.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    android.hardware.camera.device@3.6.vendor \
+    android.hardware.camera.provider@2.6.vendor \
+    android.frameworks.sensorservice@1.0.vendor
 
 # CAS
 PRODUCT_PACKAGES += \
