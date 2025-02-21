c2f5d4f996d7417e6fe8ccb1f520887b84341ea9
Aflaungos <vimavick@gmail.com>
2025-02-21 10:40:24 -0300

    extract-files: Add camera.postproc-impl to patch

--- extract-files.sh
+++ extract-files.sh
@@ -81,6 +81,9 @@ function blob_fixup() {
 	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
+	vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
 	*)
             return 1
             ;;
