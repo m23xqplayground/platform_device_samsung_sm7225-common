c33b247124e1303fbeddf782f6511fe7e45d9498
Aflaungos <vimavick@gmail.com>
2025-03-19 18:01:40 -0300

    sm7225-common: Rework codec2

--- common.mk
+++ common.mk
@@ -232,7 +232,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-     android.hardware.media.c2@1.2.vendor \
+     android.hardware.media.c2@1.0.vendor \
      libcodec2_hidl@1.0.vendor \
      libcodec2_vndk.vendor \
      libstagefright_bufferpool@2.0.1.vendor
