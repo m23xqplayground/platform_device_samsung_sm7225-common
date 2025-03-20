966ae6e3c03b07bb51bdd21b9745b3f17194ab6b
Aflaungos <vimavick@gmail.com>
2025-03-20 00:46:46 -0300

    sm7225-common: rootdir: Fix init.qti.qcv.rc path and update prop-files

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -48,7 +48,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qti.qcv.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qti.qcv.rc
+LOCAL_SRC_FILES    := etc/init.qti.qcv.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/etc/init
 include $(BUILD_PREBUILT)
 
