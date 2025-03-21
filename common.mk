2633b21537f24fcaba877d092d393376f66e222a
Aflaungos <vimavick@gmail.com>
2025-03-21 00:05:41 -0300

    sm7225-common: Disable SamsungDAP

--- common.mk
+++ common.mk
@@ -88,10 +88,7 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
-    libvolumelistener \
-    SamsungDAP
-
- TARGET_EXCLUDES_AUDIOFX := true
+    libvolumelistener
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(COMMON_PATH)/audio/configs/,$(TARGET_COPY_OUT_VENDOR)/etc)
